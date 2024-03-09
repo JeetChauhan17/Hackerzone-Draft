89160f4b372c45b650557757c345961bebd4bdf3
JeetChauhan17 <121753279+JeetChauhan17@users.noreply.github.com>
2024-03-09 20:53:28 +0530

    Add files via upload

---
+++ git-main/temp.js
@@ -0,0 +1,76 @@
+
+
+let index = 0,
+  interval = 1000;
+
+const rand = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const animate = (star) => {
+  star.style.setProperty("--star-left", `${rand(-10, 100)}%`);
+  star.style.setProperty("--star-top", `${rand(-40, 80)}%`);
+
+  star.style.animation = "none";
+  star.offsetHeight;
+  star.style.animation = "";
+};
+
+for (const star of document.getElementsByClassName("magic-star")) {
+  setTimeout(() => {
+    animate(star);
+
+    setInterval(() => animate(star), 1000);
+  }, index++ * (interval / 3));
+}
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+interval = null;
+
+document.querySelector(".head-text").onmouseover = (event) => {
+  let iteration = 0;
+
+  clearInterval(interval);
+
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if (index < iteration) {
+          return event.target.dataset.value[index];
+        }
+
+        return letters[Math.floor(Math.random() * 26)];
+      })
+      .join("");
+
+    if (iteration >= event.target.dataset.value.length) {
+      clearInterval(interval);
+    }
+
+    iteration += 1 / 3;
+  }, 30);
+};
+document.querySelector(".underline").onmouseover = (event) => {
+  let iteration = 0;
+
+  clearInterval(interval);
+
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if (index < iteration) {
+          return event.target.dataset.value[index];
+        }
+
+        return letters[Math.floor(Math.random() * 26)];
+      })
+      .join("");
+
+    if (iteration >= event.target.dataset.value.length) {
+      clearInterval(interval);
+    }
+
+    iteration += 1 / 3;
+  }, 30);
+};
